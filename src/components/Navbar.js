5ab09da84e72b0a17c2434936c62ac058b0e109b
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2022-09-01 17:44:43 -0500

    whats

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import logo from "../img/logo.svg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faWhatsapp,
